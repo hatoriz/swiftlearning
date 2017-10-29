e4f3526317c611a938f22ffcb94ee33d87bec365
hatoriz <hatoriz@nathdanais-MacBook-Pro.local>
2017-10-29 14:51:26 +0700

    [SWIFT] - Lazy Property

--- learning_swift/MyPlayground_ch3.playground/Contents.swift
+++ learning_swift/MyPlayground_ch3.playground/Contents.swift
@@ -44,7 +44,32 @@ import UIKit
 //var observer = PropertyObserverExample()
 //observer.number = 2
 //observer.number = 5
-////------ Observer ----
+////------ Observer  End ----
 
-
+////------ Lazy Property ----
+//class SomeExpensiveClass {
+//    init(id: Int) {
+//        print("Expensive class \(id) created!")
+//    }
+//}
+//
+//class LazyPropertyExample {
+//    // normal class
+//    var expensiveClass1 = SomeExpensiveClass(id: 1)
+//
+//    // lazy class
+//    lazy var expensiveClass2 = SomeExpensiveClass(id: 2)
+//
+//    init(){
+//        print ("First class created!")
+//    }
+//
+//}
+//
+//var lazyExample = LazyPropertyExample()
+//
+//lazyExample.expensiveClass1
+//lazyExample.expensiveClass2
+//
+////------ Lazy Property End ----
 
