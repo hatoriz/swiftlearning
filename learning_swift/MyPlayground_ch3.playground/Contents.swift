7d6a93113f4298fb57e99262cd8aba2b76b529aa
hatoriz <hatoriz@nathdanais-MacBook-Pro.local>
2017-10-29 14:43:46 +0700

    [SWIFT] - Observer for property

--- learning_swift/MyPlayground_ch3.playground/Contents.swift
+++ learning_swift/MyPlayground_ch3.playground/Contents.swift
@@ -2,29 +2,49 @@
 
 import UIKit
 //----------- CLASS -----
-class InitAndDeinitExample {
-    init() {
-        print("I've been created")
-    }
-    
-    convenience init? (value: Int){
-        self.init()
-        
-        if value > 5 {
-            return nil
-        }
-        else {
-            print("I was called with the convenience init")
-        }
-
-    }
-    
-    deinit {
-        print("I'm going away")
-    }
-}
-
-var example : InitAndDeinitExample?
-
-example = InitAndDeinitExample(value: 6)
+//class InitAndDeinitExample {
+//    init() {
+//        print("I've been created")
+//    }
+//
+//    convenience init? (value: Int){
+//        self.init()
+//
+//        if value > 5 {
+//            return nil
+//        }
+//        else {
+//            print("I was called with the convenience init")
+//        }
+//
+//    }
+//
+//    deinit {
+//        print("I'm going away")
+//    }
+//}
+//
+//var example : InitAndDeinitExample?
+//
+//example = InitAndDeinitExample(value: 6)
 //----------- CLASS END -----
+
+////------ Observer ----
+//class PropertyObserverExample {
+//    var number : Int = 0 {
+//        willSet(newNumber) {
+//            print("About to change to \(newNumber)")
+//        }
+//        didSet(oldNumber) {
+//            print("Just changed from \(oldNumber) to \(self.number)!")
+//        }
+//    }
+//}
+//
+//var observer = PropertyObserverExample()
+//observer.number = 2
+//observer.number = 5
+////------ Observer ----
+
+
+
