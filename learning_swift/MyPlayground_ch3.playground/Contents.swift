bc9ceb4c7c8e11836d756bd3ff36cfcc69c84341
hatoriz <hatoriz@nathdanais-MacBook-Pro.local>
2017-10-30 10:24:51 +0700

    [SWIFT] - Error Handling

--- learning_swift/MyPlayground_ch3.playground/Contents.swift
+++ learning_swift/MyPlayground_ch3.playground/Contents.swift
@@ -119,3 +119,101 @@ import UIKit
 //
 //////------ Protocol End ----
 
+//// Loading from URL
+//if let URL = URL(string: "https://oreilly.com") {
+//    let loadedDataFromURL = try? Data(contentsOf: URL)
+//}
+
+
+////---------- Error Handling ------
+//
+//enum BankError : Error {
+//    // Not enough money in the account
+//    case notEnoughFunds
+//
+//    // Can't create an account with negative money
+//    case cannotBeginWithNegativeFunds
+//
+//    // Can't make a negative deposit or withdrawal
+//    case cannotMakeNegativeTransaction(amount:Float)
+//}
+//
+//class BankAccount {
+//    private (set) var balance : Float = 0.0
+//
+//    init(amount : Float) throws {
+//
+//        guard amount > 0 else {
+//            throw BankError.cannotBeginWithNegativeFunds
+//        }
+//        balance = amount
+//    }
+//
+//    func deposit(amount : Float) throws {
+//        guard amount > 0 else {
+//            throw BankError.cannotMakeNegativeTransaction(amount: amount)
+//        }
+//
+//        balance += amount
+//    }
+//
+//    // Withdraws money from the bank account
+//    func withdraw(amount : Float) throws {
+//
+//        // Ensure that we're trying to deposit a non-negative amount
+//        guard amount > 0 else {
+//            throw BankError.cannotMakeNegativeTransaction(amount: amount)
+//        }
+//
+//        // Ensure that we have enough to withdraw this amount
+//        guard balance >= amount else {
+//            throw BankError.notEnoughFunds
+//        }
+//
+//        balance -= amount
+//    }
+//}
+//
+//do {
+//    let vacationFund = try BankAccount (amount: -5)
+//    try vacationFund.deposit(amount: 10)
+//    try vacationFund.withdraw(amount: 1)
+//    
+//} catch let error as BankError {
+//    switch error {
+//    case .notEnoughFunds :
+//        print("Not enought fund")
+//    case .cannotBeginWithNegativeFunds:
+//        print("Tried to start an account with negative money!")
+//    case .cannotMakeNegativeTransaction(let amount):
+//        print("Tried to do a transaction with a negative amount of \(amount)!")
+//    default:
+//        print("Bank Account unknow error")
+//    }
+//}
+//
+//let secretBankAccount = try? BankAccount(amount: -50)
+////---------- Error Handling Ends ------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
